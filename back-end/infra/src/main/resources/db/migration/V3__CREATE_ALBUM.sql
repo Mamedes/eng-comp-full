8860974b7cdc5c6ac52a6e71e3f57a5aa082b38a
mamedes <mamedeser@gmail.com>
2026-01-18 20:04:42 -0400

    chore: setup docker-compose and update database schema

--- back-end/infra/src/main/resources/db/migration/V3__CREATE_ALBUM.sql
+++ back-end/infra/src/main/resources/db/migration/V3__CREATE_ALBUM.sql
@@ -5,7 +5,11 @@ CREATE TABLE album (
     secure_id UUID DEFAULT uuid_generate_v4() UNIQUE,
     titulo VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    artista_id INTEGER REFERENCES artista(id) ON DELETE CASCADE
+
+
 );
 
 CREATE UNIQUE INDEX uk_album_secure_id ON album (secure_id);
+CREATE INDEX idx_album_artista_id ON album (artista_id);
