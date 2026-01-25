6b06d1c4c83825e24fcf8959ceaba909f8fb6e04
mamedes <mamedeser@gmail.com>
2026-01-24 20:25:15 -0400

    refactor:  change  folder and config vite

--- front-end/artists-manager/vite.config.ts
+++ front-end/artists-manager/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
-import path from "path";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-// Simulação de __dirname para ambientes ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,6 +12,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@core": path.resolve(__dirname, "./src/core"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
 });
