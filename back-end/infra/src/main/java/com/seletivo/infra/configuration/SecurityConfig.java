acae607af5ba6f7d680f055830e83ae0f82140c3
Mamedes Eron <mamedeser@gmail.com>
2026-01-29 10:44:52 -0400

    fix(security): allow 'refresh-token' header in CORS configuration

--- back-end/infra/src/main/java/com/seletivo/infra/configuration/SecurityConfig.java
+++ back-end/infra/src/main/java/com/seletivo/infra/configuration/SecurityConfig.java
@@ -96,8 +96,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(allowedOrigin));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token","refresh-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token","refresh-token"));
 
         configuration.setAllowCredentials(true);
 
